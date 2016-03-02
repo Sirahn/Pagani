7d8bba381703879844c0aa7f6dd014eb2bc7529f
Your Name <you@example.com>
2016-03-02 10:53:53 -0500

    setting up repo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int z = 1;
+            int w = 2;
+            int y = z + w;
+
+            Console.WriteLine(y);
+
+
+        }
+    }
+}
